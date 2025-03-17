c241cac52d58bdd58dc3f17536fcc187355829f4
Samarth Kamble <kamblesamarth04@gmail.com>
2025-03-17 21:41:30 +0530

    Intialize the file and folder struture

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import { connectDB } from "./lib/db";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 require("dotenv").config();
 
 export const app = express();
 connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.APP_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 
 // Creating a server
 app.listen(process.env.PORT, () => {
