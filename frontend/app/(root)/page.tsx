be3c9a0652c2f90f51b5d7366b995fd9f5a2958c
Samarth Kamble <kamblesamarth04@gmail.com>
2025-03-18 19:29:22 +0530

    Add authentication routes and weather dashboard route ; update layout and components

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import { Feature } from "./_components/Feature";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="py-14">
       <Hero />
+      <Feature />
     </div>
   );
 }
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${roboto.variable} antialiased`}>
+      <body className={`${roboto.variable} dark:bg-gray-950`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
